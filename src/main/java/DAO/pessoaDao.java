b01a8964127a8cac7f5b85160a37e4dffef59f48
George Neves <ge.henrique.neves@gmail.com>
2025-06-13 20:52:44 -0300

    adição de métodos extras no Dao de pessoa

--- src/main/java/DAO/pessoaDao.java
+++ src/main/java/DAO/pessoaDao.java
@@ -1,13 +1,16 @@
 package DAO;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JOptionPane;
 
 import Database.databaseconn;
 import Model.pessoa;
 
-public class pessoaDao implements dao {
+public class pessoaDao{
 
     public void cadastrar(pessoa pessoa){
         databaseconn bd = new databaseconn();
@@ -36,6 +39,83 @@ public class pessoaDao implements dao {
         }
     }
 
+    public pessoa buscar(String username) {
+        pessoa pessoa = new pessoa ();
+
+        databaseconn bd = new databaseconn();
+        PreparedStatement statement;
+        ResultSet rs = null;
+        try {
+            if(!bd.getConnection()){
+                JOptionPane.showMessageDialog(null, "Falha na conexão, o sistem será fechado!");
+                System.exit(0);
+            }
+
+            String url = this.buscarQuery();
+            statement = bd.connection.prepareStatement(url);
+            statement.setString(1, username);
+            rs = statement.executeQuery();
+
+            while (rs.next()) {
+                pessoa.setDdd(rs.getInt("DDD_Telefone"));
+                pessoa.setEmail(rs.getString("Email"));
+                pessoa.setNome(rs.getString("Nome"));
+                pessoa.setPapel(rs.getInt("Papel"));
+                pessoa.setTelefone(rs.getInt("Telefone"));
+                pessoa.setUsername(rs.getString("Nome_de_Usuario"));
+            }
+            statement.close();
+            bd.connection.close();
+        } catch(Exception erro) {
+            JOptionPane.showMessageDialog(null, "Algo de errado aconteceu no cadastro:\n " + erro.toString());
+        }
+
+        return pessoa;
+    }
+
+    public List<pessoa> buscarTodos() {
+        List<pessoa> pessoas = new ArrayList<>();
+        databaseconn bd = new databaseconn();
+        PreparedStatement statement;
+        ResultSet rs = null;
+        try {
+            if(!bd.getConnection()){
+                JOptionPane.showMessageDialog(null, "Falha na conexão, o sistem será fechado!");
+                System.exit(0);
+            }
+
+            String url = this.buscarTodosQuery();
+            statement = bd.connection.prepareStatement(url);
+            rs = statement.executeQuery();
+
+            while (rs.next()) {
+                pessoa pessoa = new pessoa ();
+                pessoa.setDdd(rs.getInt("DDD_Telefone"));
+                pessoa.setEmail(rs.getString("Email"));
+                pessoa.setNome(rs.getString("Nome"));
+                pessoa.setPapel(rs.getInt("Papel"));
+                pessoa.setTelefone(rs.getInt("Telefone"));
+                pessoa.setUsername(rs.getString("Nome_de_Usuario"));
+                pessoas.add(pessoa);
+            }
+            statement.close();
+            bd.connection.close();
+        } catch(Exception erro) {
+            JOptionPane.showMessageDialog(null, "Algo de errado aconteceu no cadastro:\n " + erro.toString());
+        }
+
+        return pessoas;
+    }
+
+     private String buscarTodosQuery() {
+        return "SELECT * FROM Pessoa";
+    }
+    
+    private String buscarQuery() {
+        return "SELECT * FROM Pessoa WHERE Nome_de_Usuario = ?";
+    }
+
+    
     private String cadastrarQuery() {
         return "INSERT INTO Pessoa (Id_Pessoa, Nome, DDD_Telefone, Telefone, Nome_de_Usuario, Senha, Email, Papel) VALUES (nextval('Pessoa_Id_Pessoa_seq'),?,?,?,?,?,?,?)";
     }
